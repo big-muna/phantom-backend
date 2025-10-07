f31ad2a55ed7259b0ddf1e1440d3213aea047c30
big-muna <nnaemekabright377@gmail.com>
2025-10-07 09:47:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -917,9 +917,8 @@ import authRoutes from "./routes/auth.js";
 app.use("/api/auth", authRoutes);
 
 // ✅ Catch-all route for frontend
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 // =====================================================================
 // ------------------------- SERVER START ------------------------------
